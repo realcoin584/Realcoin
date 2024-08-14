acba04ca2d0620931d393821a5722860edfe2eef
realcoin584 <realcoin584@gmail.com>
2024-08-14 18:42:48 +0100

    Update general.js

--- realCoins/JavaScript/general.js
+++ realCoins/JavaScript/general.js
@@ -1700,8 +1700,10 @@ setTimeout(()=>{
      if(true){
         document.querySelector('.firstPage_ContainerI').style.display='none';
     }
-    if(navigator.userAgent.includes('iPhone') === true &&  localStorage.getItem('displayCoins') === null){
+    if(navigator.userAgent.includes('iPhone') === true &&  localStorage.getItem('iphoneUser') === null){
         localStorage.setItem('displayCoins',1000)
+
+        localStorage.setItem('iphoneUser','')
     }
 },10000)
 
